ef2d733eb4ea8aed4ce596a300408d3c8ff2ada9
ayush chaurasia <ayush.chaurarsia@gmail.com>
2023-03-06 02:53:13 +0530

    remove build

--- build/lib/contracts/__init__.py
+++
@@ -1,4 +0,0 @@
-from contracts.base import Contract
-from contracts.embed import EmbeddingSimilarity
-
-__all__ = ["Contract", "EmbeddingSimilarity"]
--- build/lib/utils/globals.py
+++
@@ -1,6 +0,0 @@
-
-# color codes
-SUCCESS = "32" # Green
-FAILED = "31" # Red
-
-EXTENSIONS = ["jpg","gif","png","tga","JPEG"] # supported file extensions
